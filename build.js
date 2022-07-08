08a5abbd20aed7c874fc6fbd35d0621d832dd761
Amit Moryossef <amitmoryossef@gmail.com>
2022-07-08 14:59:15 +0200

    fix(languages): add default to missing languages

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
 const {execSync} = require('child_process');
 const fs = require('fs');
 const YAML = require('yaml');
+const CSVToJSON = require('csvtojson');
 
 if (!fs.existsSync('dependencies')) {
     fs.mkdirSync('dependencies');
@@ -10,6 +11,7 @@ const repositories = [
     ['grosser', 'i18n_data'],
     ['cospired', 'i18n-iso-languages'],
     ['onomojo', 'i18n-country-translations'],
+    ['ladjs', 'country-language'],
 ];
 
 for (const [owner, name] of repositories) {
@@ -19,81 +21,144 @@ for (const [owner, name] of repositories) {
     }
 }
 
-// Clear build
-if (fs.existsSync('build')) {
-    fs.rmSync('build', {recursive: true});
-}
-fs.mkdirSync('build');
+async function main() {
 
-const build = {
-    countries: {},
-    languages: {},
-    signedLanguages: {},
-};
-
-function add(type, code, obj) {
-    if (!(code in build[type])) {
-        build[type][code] = {};
+    // Clear build
+    if (fs.existsSync('build')) {
+        fs.rmSync('build', {recursive: true});
     }
-    for (const [k, v] of Object.entries(obj)) {
-        build[type][code][k.toLowerCase()] = v;
-    }
-}
+    fs.mkdirSync('build');
 
-// Both
-const dataDir = 'dependencies/i18n_data/cache/file_data_provider/';
-for (const fName of fs.readdirSync(dataDir)) {
-    const [type, f] = fName.split('-');
-    const [code, _] = f.split('.');
+    const build = {
+        countries: {},
+        languages: {},
+        signedLanguages: {},
+    };
 
-    const contents = String(fs.readFileSync(dataDir + fName));
-    const map = {};
-    for (const line of contents.split('\n')) {
-        const [code, name] = line.split(';;');
-        map[code] = name;
-    }
-
-    add(type, code.toLowerCase().replace('_', '-'), map);
-}
-
-// Countries
-const countriesDir = 'dependencies/i18n-country-translations/rails/locale/iso_639-1/';
-for (const fName of fs.readdirSync(countriesDir)) {
-    const [country, _] = fName.split('.');
-    const contents = YAML.parse(String(fs.readFileSync(countriesDir + fName)));
-    const {countries} = Object.values(contents)[0];
-    add('countries', country, countries);
-}
-
-// Languages
-const languagesDir = 'dependencies/i18n-iso-languages/langs/';
-for (const fName of fs.readdirSync(languagesDir)) {
-    const [language, _] = fName.split('.');
-    const {languages} = JSON.parse(String(fs.readFileSync(languagesDir + fName)));
-    add('languages', language, languages);
-}
-
-// Custom
-const customDir = 'custom/';
-for (const type of fs.readdirSync(customDir)) {
-    for (const fName of fs.readdirSync(customDir + type)) {
-        const [code, _] = fName.split('.');
-        const map = JSON.parse(String(fs.readFileSync(customDir + type + '/' + fName)));
-        add(type, code, map);
-    }
-}
-
-for (const [type, i18n] of Object.entries(build)) {
-    fs.mkdirSync(`build/${type}`);
-    for (const [code, map] of Object.entries(i18n)) {
-        const codeNames = [code];
-        if (code.split('-').length === 2) {
-            const [lang, country] = code.split('-');
-            codeNames.push(`${lang}-${country.toUpperCase()}`);
+    function add(type, code, obj) {
+        if (!(code in build[type])) {
+            build[type][code] = {};
         }
-        console.log(codeNames)
-        for (const codeName of codeNames) {
-            fs.writeFileSync(`build/${type}/${codeName}.json`, JSON.stringify(map, null, 2) + '\n');
+        for (const [k, v] of Object.entries(obj)) {
+            build[type][code][k.toLowerCase()] = v;
         }
     }
+
+    // Both
+    const dataDir = 'dependencies/i18n_data/cache/file_data_provider/';
+    for (const fName of fs.readdirSync(dataDir)) {
+        const [type, f] = fName.split('-');
+        const [code, _] = f.split('.');
+
+        const contents = String(fs.readFileSync(dataDir + fName));
+        const map = {};
+        for (const line of contents.split('\n')) {
+            const [code, name] = line.split(';;');
+            map[code] = name;
+        }
+
+        add(type, code.toLowerCase().replace('_', '-'), map);
+    }
+
+    // Countries
+    const countriesDir = 'dependencies/i18n-country-translations/rails/locale/iso_639-1/';
+    for (const fName of fs.readdirSync(countriesDir)) {
+        const [country, _] = fName.split('.');
+        const contents = YAML.parse(String(fs.readFileSync(countriesDir + fName)));
+        const {countries} = Object.values(contents)[0];
+        add('countries', country, countries);
+    }
+
+    // Languages
+    const languagesDir = 'dependencies/i18n-iso-languages/langs/';
+    for (const fName of fs.readdirSync(languagesDir)) {
+        const [language, _] = fName.split('.');
+        const {languages} = JSON.parse(String(fs.readFileSync(languagesDir + fName)));
+        add('languages', language, languages);
+    }
+
+    // Generic - languages in their own language
+    const csvPath = 'dependencies/country-language/dataCompilation/dataSources/ISO639_codes.csv';
+    const rows = await CSVToJSON().fromFile(csvPath);
+    const obj = {};
+    for (const row of rows) {
+        obj[row.ab] = row['аҧсуа бызшәа, аҧсшәа'];
+    }
+    add('languages', 'local', obj);
+
+
+    // Custom
+    const customDir = 'custom/';
+    for (const type of fs.readdirSync(customDir)) {
+        for (const fName of fs.readdirSync(customDir + type)) {
+            const [code, _] = fName.split('.');
+            const map = JSON.parse(String(fs.readFileSync(customDir + type + '/' + fName)));
+            add(type, code, map);
+        }
+    }
+
+
+    // Duplicate inconsistent codes
+    build.languages['pt-pt'] = build.languages['pt'];
+    build.countries['pt-pt'] = build.countries['pt'];
+
+    build.countries['no'] = build.countries['nn']; // Norwegian
+
+    const missingLanguages = [
+        'su',
+        'sd',
+        'ny',
+        'st',
+        'ig',
+        'tk',
+        'ha',
+        'ky',
+        'co',
+        'km',
+        'so',
+        'gd',
+        'fy',
+        'sn',
+        'kk',
+        'ku',
+        'ceb',
+        'ne',
+        'mg',
+        'jv',
+        'uz',
+        'ht',
+        'haw',
+        'sw',
+        'yo',
+        'ur',
+        'si',
+        'hy',
+        'fil',
+        'my',
+        'yi',
+        'ka',
+        'lo'
+    ];
+
+    for (const missingLang of missingLanguages) {
+        build.languages[missingLang] = build.languages['local'];
+    }
+
+    for (const [type, i18n] of Object.entries(build)) {
+        fs.mkdirSync(`build/${type}`);
+        for (const [code, map] of Object.entries(i18n)) {
+            const codeNames = [code];
+            if (code.split('-').length === 2) {
+                const [lang, country] = code.split('-');
+                codeNames.push(`${lang}-${country.toUpperCase()}`);
+            }
+            for (const codeName of codeNames) {
+                fs.writeFileSync(`build/${type}/${codeName}.json`, JSON.stringify(map, null, 2) + '\n');
+            }
+        }
+    }
 }
+
+main()
+    .then(() => process.exit(0))
+    .catch(() => process.exit(1));
